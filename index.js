1d1fd5546e1d1cf649565d94457ca6028e5cf8cc
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2024-07-16 04:28:37 +0700

    feat: fix ui main

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const openDesk = (contact) => {
               </div>
           </div>
         </div>
-        <h2 class="text-xl mt-5 font-semibold">Detail Contact</h2>
+        <h2 class="text-xl mt-5 font-semibold">Detail Contact :</h2>
         <div class="flex items-center gap-2 mt-3">
           <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -124,7 +124,7 @@ const openDesk = (contact) => {
             <path fill="#222" d="M32 38.08a8.51 8.51 0 0 1-5.13-1.71L3.52 18.71a1.5 1.5 0 1 1 1.81-2.39L28.68 34a5.55 5.55 0 0 0 6.64 0l23.35-17.68a1.5 1.5 0 1 1 1.81 2.39L37.13 36.37A8.51 8.51 0 0 1 32 38.08Z"></path>
             <path fill="#222" d="M4.17 49.14a1.5 1.5 0 0 1-1-2.62l18.4-16.41a1.5 1.5 0 0 1 2 2.24L5.17 48.76a1.46 1.46 0 0 1-1 .38zm55.66 0a1.46 1.46 0 0 1-1-.38l-18.4-16.41a1.5 1.5 0 1 1 2-2.24l18.39 16.41a1.5 1.5 0 0 1-1 2.62z"></path>
           </svg>
-          <div class="text-xl ">${contact.email}</div>
+          <div class="text-xl">${contact.email}</div>
         </div>
         <p class="text-xl">Phone: ${contact.phone}</p>
     </div>
