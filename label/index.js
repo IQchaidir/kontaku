f93ad3f368ba0eea52053647240068d3151e80eb
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2024-07-17 03:51:05 +0700

    feat: add ui modal

--- label/index.js
+++ label/index.js
@@ -1,29 +1,17 @@
 const allContactsElement = document.getElementById("allContacts")
-const addContactFormElement = document.getElementById("addContactModal")
+const addContactFormElement = document.getElementById("add-contact-form")
 const labelsContainer = document.getElementById("label-container")
-const createLabelForm = document.getElementById("createLabelForm")
 const toggleDeskButton = document.getElementById("toggleDesk")
 const deskComponent = document.getElementById("deskComponent")
 const deleteModalContact = document.getElementById("deleteModalContact")
-const createLabelModal = document.getElementById("createLabelModal")
 const deleteModalLabel = document.getElementById("deleteModalLabel")
 let currentContactId = null
 let currentLabelId = null
-
-const showAddContactModal = () => {
-    addContactFormElement.classList.toggle("hidden")
-    addContactFormElement.classList.toggle("flex")
-}
 
 const showModalDeleteContact = (contactId) => {
     currentContactId = contactId
     deleteModalContact.classList.toggle("hidden")
     deleteModalContact.classList.toggle("flex")
-}
-
-const showCreateLabelModal = () => {
-    createLabelModal.classList.toggle("hidden")
-    createLabelModal.classList.toggle("flex")
 }
 
 const showModalDeleteLabel = (labelId) => {
@@ -48,9 +36,16 @@ const confirmDeleteLabel = (x) => {
     showModalDeleteLabel()
 }
 
-const renderContacts = () => {
-    const contacts = loadContact()
-    const labels = loadLabels()
+function getCurrentLabelId() {
+    const params = new URLSearchParams(window.location.search)
+    const id = Number(params.get("id"))
+
+    return id
+}
+
+const renderContactFilterLabel = () => {
+    const labelId = getCurrentLabelId()
+    const contacts = filterContactByLabel(labelId)
     const params = new URLSearchParams(window.location.search)
     const keyword = params.get("search")
 
@@ -241,80 +236,53 @@ const renderLabels = () => {
 
     const labelItemsElements = labels.map((label) => {
         return `
-        <li class="w-full rounded-md hover:bg-indigo-200 py-2 px-4 flex justify-between">
-          <a href="/label/?id=${label.id}" class="cursor-pointer"><span>${label.name}</span></a>
-          <div class="flex space-x-5 justify-end">
-            <div class="cursor-pointer edit-contact">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil hover:scale-150">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-                    <path d="M13.5 6.5l4 4" />
-                </svg>
-            </div>
-            <div class="cursor-pointer delete-contact" onclick="showModalDeleteLabel(${label.id})">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash hover:scale-150">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 7l16 0" />
-                    <path d="M10 11l0 6" />
-                    <path d="M14 11l0 6" />
-                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                </svg>
-            </div>
+      <li class="w-full rounded-md hover:bg-indigo-200 py-2 px-4 flex justify-between">
+        <a href="/label/?id=${label.id}" class="cursor-pointer"><span>${label.name}</span></a>
+        <div class="flex space-x-5 justify-end">
+          <div class="cursor-pointer edit-contact">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil hover:scale-150">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                  <path d="M13.5 6.5l4 4" />
+              </svg>
           </div>
-        </li>`
+          <div class="cursor-pointer delete-contact" onclick="showModalDeleteLabel(${label.id})">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash hover:scale-150">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 7l16 0" />
+                  <path d="M10 11l0 6" />
+                  <path d="M14 11l0 6" />
+                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+              </svg>
+          </div>
+        </div>
+      </li>`
     })
 
     const labelItems = labelItemsElements.join("")
     labelsContainer.innerHTML = labelItems
 }
 
-const renderOptionLabels = () => {
+const addLabels = (name) => {
     const labels = loadLabels()
-    const selectElement = document.getElementById("label")
-
-    const defaultOption = document.createElement("option")
-    defaultOption.value = ""
-    defaultOption.textContent = "No Label"
-    selectElement.appendChild(defaultOption)
-
-    labels.forEach((label) => {
-        const option = document.createElement("option")
-        option.value = label.id.toString()
-        option.textContent = label.name
-        selectElement.appendChild(option)
+    const existinglabel = labels.find((label) => {
+        return label.name.toLowerCase() === name.toLowerCase()
     })
-}
 
-const addLabels = (event) => {
-    event.preventDefault()
-
-    const formData = new FormData(event.target)
-
-    const labelName = formData.get("labelName").trim()
-
-    const labels = loadLabels()
-    const existingLabel = labels.find((label) => label.name.toLowerCase() === labelName.toLowerCase())
-
-    if (existingLabel) {
-        alert("The label already exists")
-        return
+    if (existinglabel) {
+        return alert("The label already exists")
     }
-
     const newId = labels.length ? labels[labels.length - 1].id + 1 : 1
 
     const newLabel = {
         id: newId,
-        name: labelName,
+        name,
     }
-
-    const updatedLabels = [...labels, newLabel]
-    saveLabel(updatedLabels)
-
+    const updateLabels = [...labels, newLabel]
+    saveLabel(updateLabels)
     renderContacts()
     renderLabels()
-
-    showCreateLabelModal()
 }
 
 const editLabels = (id, name) => {
@@ -357,6 +325,15 @@ const deleteLabel = (id) => {
     renderContacts()
 }
 
+const filterContactByLabel = (id) => {
+    const contacts = loadContact()
+    const filteredContacts = contacts.filter((contact) => {
+        return contact.label === id
+    })
+
+    return filteredContacts
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     let contactsToSave = contacts
     let labelsToSave = labels
@@ -379,9 +356,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
     saveContact(contactsToSave)
     saveLabel(labelsToSave)
-    renderOptionLabels()
-    renderContacts()
+    renderContactFilterLabel()
     renderLabels()
 })
-
-createLabelForm.addEventListener("submit", addLabels)
